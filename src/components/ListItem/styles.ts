b8fce12a038c1e150ae5e1499a25ca8510216f7c
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-06-16 16:48:01 -0300

    stylishing input & label

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
-export const ListItemContainer = styled.div``;
+export const ListItemContainer = styled.div`
+  display: flex;
+  background: #20212c;
+  padding: 0.625rem;
+  border-radius: 0.625rem;
+  margin-bottom: 0.625rem;
+  align-items: center;
+
+  input {
+    width: 1.5625rem;
+    height: 1.5625rem;
+    margin-right: 0.3125rem;
+  }
+
+  label {
+    color: #ccc;
+  }
+`;
