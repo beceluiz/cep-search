6c144974c58c67a8b303a9f90cb5b5ce52291634
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-06-16 18:01:59 -0300

    adding risked effect

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,20 +1,31 @@
 import styled from "styled-components";
 
-export const ListItemContainer = styled.div`
-  display: flex;
-  background: #20212c;
-  padding: 0.625rem;
-  border-radius: 0.625rem;
-  margin-bottom: 0.625rem;
-  align-items: center;
+type ListContainerProps = {
+  done: boolean;
+};
 
-  input {
-    width: 1.5625rem;
-    height: 1.5625rem;
-    margin-right: 0.3125rem;
-  }
-
-  label {
-    color: #ccc;
-  }
-`;
+export const ListItemContainer = styled.div(
+  ({ done }: ListContainerProps) =>
+    `
+    margin: 0 auto;
+    display: flex;
+    background: #20212c;
+    padding: 0.625rem;
+    border-radius: 0.625rem;
+    margin-bottom: 0.625rem;
+    align-items: center;
+    max-width:980px;
+    
+  
+    input {
+      width: 1.5625rem;
+      height: 1.5625rem;
+      margin-right: 0.3125rem;
+    }
+  
+    label {
+      color: #ccc;
+      text-decoration: ${done ? "line-through" : "initial"};
+      font-family: sans-serif;
+    }`
+);
